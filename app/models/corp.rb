e0a996e4685d0a94aad7a6bc525b28b9ac677c7f
hirokif <hirokif@gmail.com>
2013-03-14 13:02:31 +0900

    utf-8 coding add

--- app/models/corp.rb
+++ app/models/corp.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Corp < ActiveRecord::Base
   attr_accessible :corp_add, :corp_fax, :corp_name, :corp_no, :corp_tel
 
@@ -8,7 +10,7 @@ class Corp < ActiveRecord::Base
   private
     #このcorpを参照しているline_itemが無いことを確認する
     def ensure_not_referenced_by_any_line_item
-      if line_item.empty
+      if line_items.empty
         return true
       else
         errors.add(:base, '品目が存在します')
