f2626c55ab26c099dcc56b6acab63ffa16c25386
hirokif <hirokif@gmail.com>
2013-04-15 21:14:35 +0900

    corps.csv取込の為seed.rb修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "csv"
+
+CSV.foreach('db/corps.csv') do |row|
+  Corp.create(
+#   :id => row[0],
+    :corp_no => row[1],
+    :corp_name => row[2],
+    :corp_add => row[3],
+    :corp_tel => row[4],
+    :corp_fax => row[5],
+#   :created_at => row[6],
+#   :updated_at => row[7],
+    :corp_mail => row[8]
+  )
+end
