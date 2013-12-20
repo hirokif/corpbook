88c989a893ebb8cd673ac067d93a8ab0ffa81007
hirokif <hirokif@gmail.com>
2013-12-20 14:15:24 +0900

    app/models/hoi.rb削除

--- app/models/hoi.rb
+++
@@ -1,6 +0,0 @@
-class Hoi < ActiveRecord::Base
-  attr_accessible :corp_no, :egr_bb, :egr_fr, :egr_or, :egr_sr, :mr, :pc, :pp, :toi_no, :note
-
-  belongs_to :corp, :foreign_key => 'corp_no'
-  belongs_to :toi, :foreign_key => 'toi_no'
-end
